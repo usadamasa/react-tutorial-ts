5be010edbb4c1cd345059ed2c9b3a36c05b9cfe4
usadamasa <usadamasa@gmail.com>
2020-02-02 19:13:43 +0900

    liftup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,101 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as ReactDOM from 'react-dom';
+import './style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+type SquareType = string | null;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+interface SquareProps {
+    value: SquareType
+
+    onClick: () => void
+}
+
+
+class Square extends React.Component<SquareProps> {
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+interface BoardState {
+    squares: SquareType[]
+}
+
+class Board extends React.Component<any, BoardState> {
+    constructor(props: SquareProps) {
+        super(props);
+        this.state = {
+            squares: Array<SquareType>(9).fill(null)
+        }
+    }
+
+    private handleClick(i: number) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    private renderSquare(i: number) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const status = 'Next player: X';
+        return (
+            <
+                div>
+                < div
+                    className="status"> {status}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game/>,
+    document.getElementById('root')
+);
